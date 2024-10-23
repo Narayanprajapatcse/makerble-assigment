92e06f590e2650c755ba44ea16828a0803e564cf
Narayanprajapatcse <kunalprajapa94@gmail.com>
2024-10-23 13:01:19 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const nameInput = document.getElementById('name');
+nameInput.addEventListener('input', () => {
+  validateName();
+});
+
+function validateName() {
+  // Your validation logic here
+}
